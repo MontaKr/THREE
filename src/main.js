33e64d448732914dee06129a1495a7a8c42bc2b1
Monta <zhoon95@gmail.com>
2025-11-30 20:45:09 +0900

    Feat : Mesh

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import "./style.css"
 import * as THREE from "three"
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100)
@@ -7,15 +13,38 @@ const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerH
 camera.position.y = 1
 camera.position.z = 5
 
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color : 0xff0000})
-const mesh = new THREE.Mesh(geometry, material) 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.castShadow = true;
+directionalLight.position.set(3,4,5);
+directionalLight.lookAt(0,0,0);
+scene.add(directionalLight)
 
+const floorGeometry = new THREE.PlaneGeometry(20,20)
+const floorMaterial = new THREE.MeshStandardMaterial({color : 0xbbbbbb})
+const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+floor.rotation.x = -Math.PI / 2;
+floor.receiveShadow = true;
+floor.castShadow = true;
+scene.add(floor)
+
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshStandardMaterial({color : 0xff0000})
+const mesh = new THREE.Mesh(geometry, material) 
+mesh.castShadow = true;
+mesh.position.y = 0.5;
 scene.add(mesh);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const capsuleGeometry = new THREE.CapsuleGeometry(1,2,20,30);
+const capsuleMaterial = new THREE.MeshStandardMaterial({color : 0xffff00})
+const capsuleMesh = new THREE.Mesh(capsuleGeometry, capsuleMaterial)
+capsuleMesh.position.set(3,1.75,0);
+capsuleMesh.castShadow = true;
+capsuleMesh.receiveShadow = true;
+scene.add(capsuleMesh);
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.update();
 
 window.addEventListener('resize',()=>{
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -24,4 +53,9 @@ window.addEventListener('resize',()=>{
   renderer.render(scene, camera)
 })
 
-renderer.render(scene, camera)
+const render = () => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render)
+}
+
+render();
