321d2439e9908cc311728c5226c01ed6b75063a1
Monta <zhoon95@gmail.com>
2025-12-06 19:25:43 +0900

    Feat : Sphere / Points

--- src/main.js
+++ src/main.js
@@ -92,6 +92,61 @@ const shapeMesh = new THREE.Mesh(shapeGeometry, shapeMaterial);
 shapeMesh.position.set(0, 1, 2);
 scene.add(shapeMesh);
 
+// 3D Star Shape
+const extrudeSettings = {
+  steps : 1,
+  depth : 0.1,
+  bevelEnabled : true,
+  bevelThickness : 0.1,
+  bevelSize : 0.3,
+  bevelSegments : 100,
+}
+
+const extrudeGeometry = new THREE.ExtrudeGeometry(starShape, extrudeSettings);
+const extrudeMaterial = new THREE.MeshStandardMaterial({color : 0x0ddaaf});
+const extrudeMesh = new THREE.Mesh(extrudeGeometry,extrudeMaterial)
+extrudeMesh.position.set(2, 1.3, 2);
+extrudeMesh.castShadow = true;
+extrudeMesh.receiveShadow = true;
+scene.add(extrudeMesh)
+
+// Sphere Geometry
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sphereMaterial = new THREE.MeshStandardMaterial({color : 0x98daaf})
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh.position.set(0, 1, -3)
+sphereMesh.castShadow = true;
+scene.add(sphereMesh)
+
+const numPoints = 1000;
+const positions = new Float32Array(numPoints * 3);
+
+for (let i=0; i<numPoints; i+=1){
+  const x = (Math.random() - 0.5) * 1;
+  const y = (Math.random() - 0.5) * 1;
+  const z = (Math.random() - 0.5) * 1;
+
+  positions[i*3] = x
+  positions[i*3 + 1] = y
+  positions[i*3 + 2] = z
+}
+
+const bufferGeometry = new THREE.BufferGeometry();
+bufferGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
+const PointsMaterial = new THREE.PointsMaterial({
+  color : 0xffff00,
+  size : 0.05
+});
+
+const point = new THREE.Points(sphereGeometry, PointsMaterial)
+point.position.set(0,0,-5)
+scene.add(point)
+
+
 // Orbit Controls
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.update();
