ed9445ea86d46618511a2bc45177ddb009447a80
ZHOON <zhoon95@gmail.com>
2025-12-26 12:55:25 +0900

    Feat : Camera

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.PCFShadowMap;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -32,7 +35,7 @@ floor.castShadow = true;
 scene.add(floor);
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xffff00 });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 boxMesh.castShadow = true;
 boxMesh.receiveShadow = true;
@@ -44,16 +47,26 @@ scene.add(boxMesh);
 // scene.add(ambientLight)
 
 // DirectionalLight
-// const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-// directionalLight.castShadow = true;
-// directionalLight.position.set(3, 4, 5);
-// directionalLight.lookAt(0, 0, 0);
-// scene.add(directionalLight);
-// const directionalLightHelper = new THREE.DirectionalLightHelper(
-//   directionalLight,
-//   1
-// );
-// scene.add(directionalLightHelper);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.castShadow = true;
+directionalLight.position.set(3, 4, 5);
+directionalLight.lookAt(0, 0, 0);
+directionalLight.shadow.mapSize.width = 4096;
+directionalLight.shadow.mapSize.height = 4096;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.right = 2;
+
+directionalLight.shadow.camera.near = 0.1;
+directionalLight.shadow.camera.far = 100;
+
+scene.add(directionalLight);
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  1
+);
+scene.add(directionalLightHelper);
 
 // HemisphereLight
 // const hemisphereLight = new THREE.HemisphereLight(0xb4a912, 0x12f34f, 5);
@@ -76,18 +89,18 @@ scene.add(boxMesh);
 // const rectAreaLight = new THREE.RectAreaLight(0xffffff,5,2,2)
 // rectAreaLight.position.set(0,1,2)
 // scene.add(rectAreaLight)
-const targetObj = new THREE.Object3D();
-scene.add(targetObj);
+// const targetObj = new THREE.Object3D();
+// scene.add(targetObj);
 
-const spotLight = new THREE.SpotLight(0xffffff, 10, 100, Math.PI / 4, 1, 1);
-spotLight.castShadow = true;
-spotLight.position.set(0, 3, 0);
-spotLight.target = targetObj;
-spotLight.target.position.set(1, 0, 2);
-scene.add(spotLight);
+// const spotLight = new THREE.SpotLight(0xffffff, 10, 100, Math.PI / 4, 1, 1);
+// spotLight.castShadow = true;
+// spotLight.position.set(0, 3, 0);
+// spotLight.target = targetObj;
+// spotLight.target.position.set(1, 0, 2);
+// scene.add(spotLight);
 
-const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(spotLightHelper);
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
 
 // // Orbit Controls
 const orbitControls = new OrbitControls(camera, renderer.domElement);
